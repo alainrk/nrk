6e5b299bc8cd6211e502507c3ff7d7a5cfaad4a7
Alain <alain.devcs@gmail.com>
2025-04-06 16:35:00 +0200

    typo

--- src/object.h
+++ src/object.h
@@ -23,7 +23,7 @@ typedef enum {
 // We use a kind "Type Punning", (in this case Nystrom calls it Struct
 // Injeritance). Using the mandatory C alignment of the first element in struct,
 // we are sure that Obj will be at the beginning, this allows some kind of
-// "inheritance". So we can take a struct pointer and convert it to it's first
+// "inheritance". So we can take a struct pointer and convert it to its first
 // element's pointer (and viceversa) => i.e. upcasting/downcasting it's possible
 // this way.
 // Obj will be always the first element in every struct ObjXXX.
